8b548fafaf35ea8c23859a58c4b18bac61ca4fa9
simanto-steve-sb <simanto.steve.sbl@outlook.comm>
2023-07-22 16:08:01 +0600

    wsdfsadf

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+// let userName = 'tormuj@gmail.com'
+
+let isLoggedIn = false;
+function LogIn() {
+  
+  if (isLoggedIn) {
+    return <div> <h1>User Logged in</h1></div>
+  }
+  return <div> <h1>Please Log in</h1></div>
+} 
+
+function submitHandler() {
+  document.getElementById('logVal').value = ''
+}
+
+
+function App() {
+  return (
+    <div className="App">
+     <input id='logVal' type="text"  />
+     <div> <button onClick={submitHandler} >Submit</button></div>
+    
+      <LogIn isLoggedIn={''} />
+    </div>
+  );
+}
+
+export default App;
