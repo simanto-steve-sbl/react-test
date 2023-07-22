51d033a12cc44abf795edbcc4dddb10321114aa1
simanto-steve-sb <simanto.steve.sbl@outlook.comm>
2023-07-22 18:33:06 +0600

    hiiiiii

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
+import AttendanceTable from './components/AttendanceTable';
 
-// let userName = 'tormuj@gmail.com'
 
-let isLoggedIn = false;
-function LogIn() {
-  
-  if (isLoggedIn) {
-    return <div> <h1>User Logged in</h1></div>
-  }
-  return <div> <h1>Please Log in</h1></div>
-} 
-
-function submitHandler() {
-  document.getElementById('logVal').value = ''
-}
 
 
 function App() {
   return (
     <div className="App">
-     <input id='logVal' type="text"  />
-     <div> <button onClick={submitHandler} >Submit</button></div>
-      <h1>Helloooooooooo</h1>
-      <LogIn isLoggedIn={''} />
+      <AttendanceTable/>
     </div>
   );
 }
---
+++ src/components/AttendanceTable.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const AttendanceTable = () => {
+  const [students, setStudents] = useState([
+    { id: 1, name: "John Doe", present: false, absent: false },
+    { id: 2, name: "Jane Smith", present: false, absent: false },
+    // Add more students here if needed
+  ]);
+
+  const handleAttendanceChange = (index, field) => {
+    const updatedStudents = [...students];
+    updatedStudents[index][field] = !updatedStudents[index][field];
+    setStudents(updatedStudents);
+  };
+
+  return (
+    <div>
+      <h1>Attendance Table</h1>
+      <table>
+      <thead>
+        <tr>
+          <th>Student ID</th>
+          <th>Student Name</th>
+          <th>Present</th>
+          <th>Absent</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student, index) => (
+          <tr key={student.id}>
+            <td>{student.id}</td>
+            <td>{student.name}</td>
+            <td>
+              <input
+                type="checkbox"
+                checked={student.present}
+                onChange={() => handleAttendanceChange(index, "present")}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={student.absent}
+                onChange={() => handleAttendanceChange(index, "absent")}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table></div>
+    
+  );
+};
+
+export default AttendanceTable;
